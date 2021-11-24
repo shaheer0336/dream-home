4d6cd05eec40f2d7566ec00539049245bda768b5
shaheer0336 <shaheeralee0336@gmail.com>
2021-11-25 01:39:36 +0500

    work complete

---
+++ app.js
@@ -0,0 +1,11 @@
+
+
+function expFunc() {
+    var expo = document.getElementById('experience').innerHTML;
+    console.log(expo);
+    console.log("hola")
+}
+
+expFunc();
+console.log("qwertyhelloi")
+console.log("hello")
